047b605cf8ad017fd68ca1ea34e3a739e94eeb85
Unic-X <armansinghkshatri69@gmail.com>
2023-06-07 18:46:36 +0530

    Map now has marker and current position

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# arogya_direct
+# Arogya Direct
 
-A new Flutter project.
+A flutter 
--- lib/Screens/map.dart
+++ lib/Screens/map.dart
@@ -13,18 +13,22 @@ class _MapScreenState extends State<MapScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
+  LatLng currentLocation = const LatLng(21.1283, 81.7663);
+  late BitmapDescriptor currentIcon = BitmapDescriptor.defaultMarker;
+
   Set<Circle> circles_ = Set.from([
     Circle(
       circleId: CircleId("test"),
-      center: LatLng(37.422131, -122.084801),
+      center: LatLng(21.1283, 81.7663),
       radius: 4000,
       strokeWidth: 2,
       fillColor: Color.fromARGB(50, 229, 154, 3),
-    )
+    ),
   ]);
 
   @override
   void initState() {
+    setMarker();
     _locationListner();
     super.initState();
   }
@@ -43,18 +47,28 @@ class _MapScreenState extends State<MapScreen> {
     }
 
     LocationSettings locationSettings = const LocationSettings(
-      accuracy: LocationAccuracy.high,
-      distanceFilter: 10,
+      accuracy: LocationAccuracy.best,
+      distanceFilter: 5,
     );
 
     Geolocator.getPositionStream(locationSettings: locationSettings)
         .listen((Position? position) async {
-      print('${position!.latitude}   ${position!.longitude}');
       await controller
           .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-        target: LatLng(position.latitude, position.longitude),
-        zoom: 14,
+        target: LatLng(position!.latitude, position.longitude),
+        zoom: 17,
       )));
+      setState(() {
+        currentLocation = LatLng(position.latitude, position.longitude);
+      });
+    });
+  }
+
+  void setMarker() async {
+    await BitmapDescriptor.fromAssetImage(
+            ImageConfiguration.empty, 'assets/marker.png')
+        .then((icon) {
+      currentIcon = icon;
     });
   }
 
@@ -64,11 +78,39 @@ class _MapScreenState extends State<MapScreen> {
       body: GoogleMap(
         mapType: MapType.normal,
         compassEnabled: true,
+        myLocationButtonEnabled: true,
         initialCameraPosition: CameraPosition(
           target: initialLocation,
           zoom: 14,
         ),
-        circles: circles_,
+        markers: {
+          Marker(
+              markerId: MarkerId("currentPos"),
+              position:
+                  LatLng(currentLocation.latitude, currentLocation.longitude),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueAzure))
+        },
+        circles: currentLocation == null
+            ? circles_
+            : {
+                Circle(
+                  circleId: CircleId("test3"),
+                  center: LatLng(
+                      currentLocation!.latitude!, currentLocation!.longitude!),
+                  radius: 100,
+                  strokeWidth: 0,
+                  fillColor: Color.fromARGB(31, 1, 134, 251),
+                ),
+                Circle(
+                  circleId: CircleId("test2"),
+                  center: LatLng(
+                      currentLocation!.latitude!, currentLocation!.longitude!),
+                  radius: 40,
+                  strokeWidth: 0,
+                  fillColor: Color.fromARGB(53, 4, 123, 241),
+                ),
+              },
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
