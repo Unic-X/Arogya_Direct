abca22803331bf33bcdc928a396756fd85105365
swati323ch <chittella7@gmail.com>
2023-06-23 21:16:48 +0530

    added samosa

--- lib/Screens/map.dart
+++ lib/Screens/map.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:html';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:arogya_direct/Screens/polygon.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
-//import 'package:arogya_direct/screens/insert_data.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:arogya_direct/screens/map.dart';
  
 
 void main() async {
